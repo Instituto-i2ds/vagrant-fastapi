252f9493beaf858fb5da221281343733c3070304
galvarado <guillermoalvarado89@gmail.com>
2021-01-14 19:51:54 -0600

    Adding POST, GET methods

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+agenda = [{'Name': 'Luis', 'Phone': 5555555555}]
+
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def hello():
     return {"Message": "Hello World from FastAPI"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/agenda")
+def read_agenda():
+    return {"agenda": agenda}
+
+class Contact(BaseModel):
+    name: str
+    phone: int
+
+@app.post("/contact")
+async def create_contact(contact: Contact):
+    contact_dict = contact.dict()
+    agenda.append(contact_dict)
+    return contact_dict
